05156ab310b570a9e6d8d04a71ed7c795ca83e1e
Vishwajith L K <vishwajithcodes@gmail.com>
2026-01-07 00:36:07 +0530

    Replace secret key and email credentials
    Updated Django settings with placeholder values for sensitive information.

--- anomaly_service/anomaly_service/settings.py
+++ anomaly_service/anomaly_service/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9a%*)14fmydqv1$=nli!qou2!be*t#!a^li)xtsc977co5mf9_'
+SECRET_KEY = '<Secret_Key>'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -122,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
