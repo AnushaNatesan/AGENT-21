a55e3a22430dd00716221f1e5eb69065dcdaec2a
R-Jayasree <jayasree2370002@ssn.edu.in>
2026-01-07 11:32:53 +0530

    Replace Supabase credentials with placeholders
    Updated Supabase URL and key placeholders for security.

--- Agent_21/apis/insert_agent_log.py
+++ Agent_21/apis/insert_agent_log.py
@@ -3,8 +3,8 @@ from django.views.decorators.csrf import csrf_exempt
 import json
 from supabase import create_client
 
-SUPABASE_URL = "https://ubvcncqceakcmosxjkpx.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVidmNuY3FjZWFrY21vc3hqa3B4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc0NTAyODAsImV4cCI6MjA4MzAyNjI4MH0.k4HrBg0-s424zl1-em8Nj4vDLPRtFb6Ad8UxBIZM1m0"
+SUPABASE_URL = <URL>
+SUPABASE_KEY = <KEY>
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 @csrf_exempt
@@ -22,3 +22,4 @@ def insert_agent_log(request):
     }).execute()
 
     return JsonResponse({"status": "success"})
+
